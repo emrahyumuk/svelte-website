6c6e02cf4549e2be8b1737f1e341a0c46f1b32a0
Emrah Yumuk <emrahyumuk@gmail.com>
2019-10-20 14:48:57 +0300

    api

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,81 @@
+<script context="module">
+  export async function preload(page, session) {
+    const authRes = await axios.post(
+      "https://strapi-mra-test.herokuapp.com/auth/local",
+      {
+        identifier: "test@test.com",
+        password: "test1234"
+      }
+    );
+    const token = authRes.data.jwt;
+    const pageData = await axios.get(
+      "https://strapi-mra-test.herokuapp.com/pages",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+
+    return { content: pageData.data[0].content };
+  }
+</script>
+
+<script>
+  import { onMount } from "svelte";
+  import axios from "axios";
+  export let content;
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
 <figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>HIGH FIVE!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>
+  <strong>
+    {@html content}
+  </strong>
+</p>
